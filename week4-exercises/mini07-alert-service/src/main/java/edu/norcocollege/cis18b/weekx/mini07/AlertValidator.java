f95b5182cab68ecc6b4b3b2b403ed640f99d9b40
TkChipChipLeader89 <jonathansotelo99@gmail.com>
2026-03-25 03:57:59 +0000

    Completed AlertValidator & InMemoryAlertRespository

--- week4-exercises/mini07-alert-service/src/main/java/edu/norcocollege/cis18b/weekx/mini07/AlertValidator.java
+++ week4-exercises/mini07-alert-service/src/main/java/edu/norcocollege/cis18b/weekx/mini07/AlertValidator.java
@@ -3,5 +3,15 @@ package edu.norcocollege.cis18b.weekx.mini07;
 public class AlertValidator {
     public void validate(Alert alert) throws InvalidAlertException {
         // TODO: Validate alert fields.
+        //Validations
+        if (alert.message() == null || alert.message().isBlank()) {
+            throw new InvalidAlertException("Message cannot be null or blank");
+        }
+        if (alert.level() == null) {
+            throw new InvalidAlertException("Alert level cannot be null");
+        }
+        if (alert.id() <= 0) {
+            throw new InvalidAlertException("ID must be a positive integer");
+        }
     }
 }
