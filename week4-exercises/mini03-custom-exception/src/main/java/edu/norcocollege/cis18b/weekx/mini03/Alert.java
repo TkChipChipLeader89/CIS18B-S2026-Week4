4ba0db3444c20169228bd338d7637d87d4e08614
TkChipChipLeader89 <jonathansotelo99@gmail.com>
2026-03-25 03:19:59 +0000

    Modified Alert & AlertLevel Note: Alert Added Vars & Getters/Setters. Changed AlertLevel to Enum

--- week4-exercises/mini03-custom-exception/src/main/java/edu/norcocollege/cis18b/weekx/mini03/Alert.java
+++ week4-exercises/mini03-custom-exception/src/main/java/edu/norcocollege/cis18b/weekx/mini03/Alert.java
@@ -1,13 +1,35 @@
 package edu.norcocollege.cis18b.weekx.mini03;
 
 public class Alert {
-    /** TODO: Add id, message, and level to the Alert class */
+    //Variables for id, message, and level
+    private int id;
+    private String message;
+    private AlertLevel level;
     
+    //Constructor for Alert class
     public Alert(int id, String message, AlertLevel level) {
         this.id = id;
         this.message = message;
         this.level = level;
     }
 
-    /* TODO: Create getters and setters for this as well */
+    /*Created getters and setters for all variables */
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public AlertLevel getLevel() {
+        return level;
+    }
+    public void setLevel(AlertLevel level) {
+        this.level = level;
+    }
 }
